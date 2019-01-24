ca22a4a528561899e077030a972655bcff994728
James Tranovich <jtranovich@gmail.com>
2019-01-24 15:20:14 -0800

    test nix script

---
+++ test/emacs.nix
@@ -0,0 +1,23 @@
+/*
+This is a nix expression to build Emacs and some Emacs packages I like
+from source on any distribution where Nix is installed. This will install
+all the dependencies from the nixpkgs repository and build the binary files
+without interfering with the host distribution.
+
+To build the project, type the following from the current directory:
+
+$ nix-build emacs.nix
+
+To run the newly compiled executable:
+
+$ ./result/bin/emacs
+*/
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  myEmacs = pkgs.emacs;
+  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
+in
+  emacsWithPackages (epkgs: (with epkgs.melpaPackages; [
+    merlin
+  ]))
