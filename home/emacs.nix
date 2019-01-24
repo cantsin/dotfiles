4711f0c89040315206fb2d11e5ae16025f1fd48d
James Tranovich <jtranovich@gmail.com>
2019-01-24 13:33:39 -0800

    updated home configuration

--- home/emacs.nix
+++ home/emacs.nix
@@ -1,139 +1,131 @@
-/*
-This is a nix expression to build Emacs and some Emacs packages I like
-from source on any distribution where Nix is installed. This will install
-all the dependencies from the nixpkgs repository and build the binary files
-without interfering with the host distribution.
-
-To build the project, type the following from the current directory:
-
-$ nix-build emacs.nix
-
-To run the newly compiled executable:
-
-$ ./result/bin/emacs
-*/
-{ pkgs ? import <nixpkgs> {} }:
-
-let
-  myEmacs = pkgs.emacs;
-  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
-in
-  emacsWithPackages (epkgs: (with epkgs.melpaPackages; [
-    alchemist
-    auto-yasnippet
-    autodisass-llvm-bitcode
-    cargo
-    cask
-    caskxy
-    cider
-    company
-    company-emacs-eclim
-    company-ghc
-    deft
-    diminish
-    dockerfile-mode
-    easy-kill
-    easy-kill-extras
-    eclim
-    elm-mode
-    ember-mode
-    ember-yasnippets
-    epl
-    erc-image
-    eshell-prompt-extras
-    esup
-    esxml
-    expand-region
-    f
-    flycheck
-    flycheck-cask
-    flycheck-color-mode-line
-    flycheck-haskell
-    flycheck-rust
-    flymake-lua
-    format-sql
-    fsharp-mode
-    gitconfig
-    gitignore-mode
-    glsl-mode
-    go-mode
-    handlebars-mode
-    haskell-mode
-    helm
-    helm-company
-    helm-dash
-    helm-git-grep
-    helm-gtags
-    helm-hoogle
-    helm-idris
-    helm-make
-    helm-projectile
-    helm-spaces
-    helm-swoop
-    ht
-    htmlize
-    hydra
-    idris-mode
-    jedi
-    jinja2-mode
-    js2-mode
-    json-mode
-    key-chord
-    kv
-    ledger-mode
-    /* lua-mode */ /* cannot build dependency? */
-    magit
-    markdown-mode
-    markdown-mode-plus
-    mc-extras
-    /* merlin */ /* broken? */
-    moe-theme
-    multiple-cursors
-    nix-mode
-    nodejs-repl
-    ocodo-svg-modelines
-    org-bullets
-    org-journal
-    package-build
-    pallet
-    pandoc-mode
-    paradox
-    paredit
-    pdf-tools
-    pkg-info
-    popup
-    powerline
-    pretty-mode
-    prodigy
-    projectile
-    puppet-mode
-    purescript-mode
-    reason-mode
-    restclient
-    rust-mode
-    s
-    scala-mode
-    scss-mode
-    session
-    smart-compile
-    smartparens
-    solarized-theme
-    stripe-buffer
-    swiper-helm
-    tagedit
-    terraform-mode
-    tide
-    toml-mode
-    tuareg
-    typescript-mode
-    typo
-    use-package
-    virtualenvwrapper
-    wc-mode
-    web-mode
-    wgrep
-    whitespace-cleanup-mode
-    writegood-mode
-    yaml-mode
-    yasnippet
-  ]))
+{ pkgs ? import <nixpkgs> {} }: epkgs:
+  let
+    luaModeOverride = epkgs.lua-mode.override (args: {
+        melpaBuild = drv: args.melpaBuild (drv // {
+          src = pkgs.fetchFromGitHub {
+                owner = "immerrr";
+                repo = "lua-mode";
+                rev = "95c64bb5634035630e8c59d10d4a1d1003265743";
+                sha256 = "0cawb544qylifkvqads307n0nfqg7lvyphqbpbzr2xvr5iyi4901";
+            };
+        });
+     });
+  in
+(with epkgs.melpaPackages; [
+  alchemist
+  auto-yasnippet
+  autodisass-llvm-bitcode
+  cargo
+  cask
+  caskxy
+  cider
+  company
+  company-emacs-eclim
+  company-ghc
+  deft
+  diminish
+  dockerfile-mode
+  easy-kill
+  easy-kill-extras
+  eclim
+  elm-mode
+  ember-mode
+  ember-yasnippets
+  epl
+  erc-image
+  eshell-prompt-extras
+  esup
+  esxml
+  expand-region
+  f
+  flycheck
+  flycheck-cask
+  flycheck-color-mode-line
+  flycheck-haskell
+  flycheck-rust
+  flymake-lua
+  format-sql
+  fsharp-mode
+  gitconfig
+  gitignore-mode
+  glsl-mode
+  go-mode
+  handlebars-mode
+  haskell-mode
+  helm
+  helm-company
+  helm-dash
+  helm-git-grep
+  helm-gtags
+  helm-hoogle
+  helm-idris
+  helm-make
+  helm-projectile
+  helm-spaces
+  helm-swoop
+  ht
+  htmlize
+  hydra
+  idris-mode
+  jedi
+  jinja2-mode
+  js2-mode
+  json-mode
+  key-chord
+  kv
+  ledger-mode
+  luaModeOverride
+  magit
+  markdown-mode
+  markdown-mode-plus
+  mc-extras
+  /* merlin */ /* broken? */
+  moe-theme
+  multiple-cursors
+  nix-mode
+  nodejs-repl
+  ocodo-svg-modelines
+  org-bullets
+  org-journal
+  package-build
+  pallet
+  pandoc-mode
+  paradox
+  paredit
+  pdf-tools
+  pkg-info
+  popup
+  powerline
+  pretty-mode
+  prodigy
+  projectile
+  puppet-mode
+  reason-mode
+  restclient
+  rust-mode
+  s
+  scala-mode
+  scss-mode
+  session
+  smart-compile
+  smartparens
+  solarized-theme
+  stripe-buffer
+  swiper-helm
+  tagedit
+  terraform-mode
+  tide
+  toml-mode
+  tuareg
+  typescript-mode
+  typo
+  use-package
+  virtualenvwrapper
+  wc-mode
+  web-mode
+  wgrep
+  whitespace-cleanup-mode
+  writegood-mode
+  yaml-mode
+  yasnippet
+])
--- home/home.nix
+++ home/home.nix
@@ -13,6 +13,7 @@ in
     pstree
     bc
     tmux
+    ispell
     byobu
   ];
 
@@ -24,6 +25,9 @@ in
   };
 
   programs.zsh.enable = true;
+  programs.zsh.initExtra = ''
+  '';
+
   programs.zsh.oh-my-zsh = {
     enable = true;
     plugins = [ "git" "docker" ];
@@ -32,127 +36,10 @@ in
 
   programs.emacs = {
     enable = true;
-    extraPackages = epkgs: (with epkgs.melpaPackages; [
-      alchemist
-      auto-yasnippet
-      autodisass-llvm-bitcode
-      cargo
-      cask
-      caskxy
-      cider
-      company
-      company-emacs-eclim
-      company-ghc
-      deft
-      diminish
-      dockerfile-mode
-      easy-kill
-      easy-kill-extras
-      eclim
-      elm-mode
-      ember-mode
-      ember-yasnippets
-      epl
-      erc-image
-      eshell-prompt-extras
-      esup
-      esxml
-      expand-region
-      f
-      flycheck
-      flycheck-cask
-      flycheck-color-mode-line
-      flycheck-haskell
-      flycheck-rust
-      flymake-lua
-      format-sql
-      fsharp-mode
-      gitconfig
-      gitignore-mode
-      glsl-mode
-      go-mode
-      handlebars-mode
-      haskell-mode
-      helm
-      helm-company
-      helm-dash
-      helm-git-grep
-      helm-gtags
-      helm-hoogle
-      helm-idris
-      helm-make
-      helm-projectile
-      helm-spaces
-      helm-swoop
-      ht
-      htmlize
-      hydra
-      idris-mode
-      jedi
-      jinja2-mode
-      js2-mode
-      json-mode
-      key-chord
-      kv
-      ledger-mode
-      /* lua-mode */ /* cannot build dependency? */
-      magit
-      markdown-mode
-      markdown-mode-plus
-      mc-extras
-      /* merlin */ /* broken? */
-      moe-theme
-      multiple-cursors
-      nix-mode
-      nodejs-repl
-      ocodo-svg-modelines
-      /* org */ /* not found? */
-      org-bullets
-      org-journal
-      package-build
-      pallet
-      pandoc-mode
-      paradox
-      paredit
-      pdf-tools
-      pkg-info
-      popup
-      powerline
-      pretty-mode
-      prodigy
-      projectile
-      puppet-mode
-      purescript-mode
-      reason-mode
-      restclient
-      rust-mode
-      s
-      scala-mode
-      scss-mode
-      session
-      smart-compile
-      smartparens
-      solarized-theme
-      stripe-buffer
-      swiper-helm
-      tagedit
-      terraform-mode
-      tide
-      toml-mode
-      tuareg
-      typescript-mode
-      typo
-      use-package
-      virtualenvwrapper
-      wc-mode
-      web-mode
-      wgrep
-      whitespace-cleanup-mode
-      writegood-mode
-      yaml-mode
-      yasnippet
-    ]);
+    extraPackages = import ./emacs.nix { inherit pkgs; };
   };
+
+  services.network-manager-applet.enable = true;
 
   home.file = {
     ".Xmodmap" = {
