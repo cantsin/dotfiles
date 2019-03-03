e116bd4ef783297b5188a8f692b9a87e0fd1c2c8
James Tranovich <jtranovich@gmail.com>
2019-03-02 20:15:38 -0800

    tweak

--- emacs.d/lisp/setup-keys.el
+++ emacs.d/lisp/setup-keys.el
@@ -211,7 +211,7 @@ prefix argument."
 (global-set-key (kbd "C-M-t") 'transpose-sexps)
 
 ;; quick search.
-(global-set-key (kbd "M-g s") 'helm-rg)
+(global-set-key (kbd "M-g s") 'helm-projectile-rg)
 
 ;; pretty-print evals.
 (global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
