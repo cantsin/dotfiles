42970a1bdfcaa678e520cd06d145b49ba62b9347
James Tranovich <jtranovich@gmail.com>
2019-03-18 16:19:04 -0700

    more openssh configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -71,6 +71,8 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";
+  services.openssh.passwordAuthentication = false;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
