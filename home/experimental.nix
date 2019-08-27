a88d4d0a37b2a18f84c5ce53567ed6ebb75b71d5
James Tranovich <jtranovich@gmail.com>
2019-08-26 21:20:14 -0700

    nixfmt (experimental)

--- home/experimental.nix
+++ home/experimental.nix
@@ -1,5 +1,12 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 let
+  nixfmt = import (pkgs.fetchFromGitHub {
+    owner = "serokell";
+    repo = "nixfmt";
+    rev = "740ad29db77ede6cf7b052ff8f170b0b1163d619";
+    sha256 = "0ww96d5yyhl4akcwr9nbgq5kvm7j3q08ikwyqwf3ivil4a0wqc5k";
+    # date = 2019-08-25T19:42:47+02:00;
+  }) { };
   catt = pkgs.python37.pkgs.buildPythonPackage rec {
     pname = "catt";
     version = "0.9.2";
@@ -22,4 +29,4 @@ let
         "Cast All The Things allows you to send videos from many, many online sources to your Chromecast.";
     };
   };
-in { home.packages = [ catt ]; }
+in { home.packages = [ catt nixfmt ]; }
