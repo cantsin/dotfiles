084f2d2f0ee56a24c0a88ef64ce83da1a46065bd
James Tranovich <jtranovich@gmail.com>
2020-03-29 16:40:19 -0700

    update org-task slightly

--- home/org-task/org-task.sh
+++ home/org-task/org-task.sh
@@ -1,3 +1,8 @@
 ID=`/run/current-system/sw/bin/id -u`
-RESULT=`TMPDIR=/run/user/$ID ~/.nix-profile/bin/emacsclient --eval '(progn (load-file "~/.config/nixpkgs/org-task/org-task.el") (get-current-task))'`
-echo ${RESULT:1:-1}
+RESULT=`TMPDIR=/run/user/$ID ~/.nix-profile/bin/emacsclient --eval '(progn (load-file "~/.config/nixpkgs/org-task/org-task.el") (get-current-task))' 2>/dev/null`
+if [ $? -ne 0 ]
+then
+    echo "emacs server is not running"
+else
+    echo ${RESULT:1:-1}
+fi
